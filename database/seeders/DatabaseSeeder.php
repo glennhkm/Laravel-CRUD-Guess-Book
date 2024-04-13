988010334b6b9e2076d4726fc770b5cd65d6d0d9
glennhkm <glenn.hkm@gmail.com>
2024-04-13 09:26:41 +0700

    chore: delete unnecessary files

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,8 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'glennhkm',
-            'email' => 'glenn.hkm@gmail.com',
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
             'password' => bcrypt('admin'),
         ]);
     }
