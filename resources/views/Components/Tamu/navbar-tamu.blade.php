372812fda17c4cfcdbca8d319e0703c9014da494
glennhkm <glenn.hkm@gmail.com>
2024-04-14 10:08:20 +0700

    feat: responsive design for chrome

--- resources/views/Components/Tamu/navbar-tamu.blade.php
+++ resources/views/Components/Tamu/navbar-tamu.blade.php
@@ -1,15 +1,15 @@
-<div class="w-full h-16 xl:h-20 2xl:h-24 bg-white shadow-md text-black flex justify-between px-14 items-center">
-    <a class="hover:text-[#e3b04b] font-bold hover:scale-110 duration-150 text-lg 2xl:text-xl" href="/">ATHAR RAYYAN M</a>
+<div class="w-full h-16 chrome-md:h-20 chrome:h-24 bg-white shadow-md text-black flex justify-between px-14 items-center">
+    <a class="hover:text-[#e3b04b] font-bold hover:scale-110 duration-150 text-lg chrome:text-xl" href="/">ATHAR RAYYAN M</a>
     <ul class=" list-none flex gap-10 items-center">
         <li class="hover:scale-110 duration-150">
-            <a class="hover:text-[#e3b04b] font-semibold text-sm xl:text-base 2xl:text-xl {{request()->is('/') ? 'text-[#e3b04b]' : ''}}" href="/">Reservation</a>
+            <a class="hover:text-[#e3b04b] font-semibold text-sm chrome-md:text-base chrome:text-xl {{request()->is('/') ? 'text-[#e3b04b]' : ''}}" href="/">Reservation</a>
         </li>
         <li class="hover:scale-110 duration-150">
-            <a class="hover:text-[#e3b04b] font-semibold text-sm xl:text-base 2xl:text-xl {{request()->is('guest-list') ? 'text-[#e3b04b]' : ''}}" href="/guest-list">Guest List</a>
+            <a class="hover:text-[#e3b04b] font-semibold text-sm chrome-md:text-base chrome:text-xl {{request()->is('guest-list') ? 'text-[#e3b04b]' : ''}}" href="/guest-list">Guest List</a>
         </li>
         <li class="hover:scale-[1.03] duration-150">
             <a class=" font-semibold text-white" href="/admin">
-                <button class="hover:bg-[#C99B3D] bg-[#e3b04b] flex justify-center gap-2 px-3 xl:px-[1.5rem] py-1.5 2xl:px-[1.8rem] xl:py-2 2xl:py-3 rounded-md items-center text-sm xl:text-base 2xl:text-xl">
+                <button class="hover:bg-[#C99B3D] bg-[#e3b04b] flex justify-center gap-2 px-3 lg:px-[2rem] py-1.5 chrome:px-[1.8rem] chrome-md:py-2 chrome:py-3 rounded-md items-center text-sm chrome-md:text-base chrome:text-xl">
                     Admin
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                         stroke="currentColor" class="w-5 h-5 text-white">
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,12 @@ export default {
     extend: {
       boxShadow: {
         'rata': '0px 5px 65px 10px rgba(0, 0, 0, 0.1)',
-      }
+      },
+      screens: {
+        // 'chrome-sm': '1280px',
+        'chrome-md': '1350px',
+        'chrome': '1700px',
+      },
     },
   },
   plugins: [
